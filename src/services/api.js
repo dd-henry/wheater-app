a07a94230f366fa2d14cbe0db95d6e7ff4038e41
dd-henry <henry.azevedo06@gmail.com>
2024-09-19 17:48:07 -0300

    fix http

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const KEY = "27e1f8b857294f80929184502241909";
 
 const fetchData = async (city) => {
-  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
   const fetchResponse = await fetch(url);
   const data = await fetchResponse.json();
